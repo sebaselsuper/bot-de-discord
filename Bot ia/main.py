8a77c94aef90918c5bb0e1d9799a03d36e8b9ba2
sebaselsuper <naweclan@gmail.com>
2026-04-07 14:17:11 -0700

    Add image handling and model prediction command

--- Bot ia/main.py
+++ Bot ia/main.py
@@ -2,8 +2,9 @@ import discord
 import random
 from discord.ext import commands
 from flask import ctx
+import requests
 
-
+from model import get_class
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -21,16 +22,16 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 @bot.command()
-async def cargar(ctx):
+async def chequeo(ctx):
     if ctx.message.attachments:
         for attachment in ctx.message.attachments:
-            if attachment.content_type and attachment.content_type.startswith('image/'):
-                await ctx.send(f"Aquí está tu imagen y la URL: {attachment.url}")
-            else:
-                await ctx.send("Eso no es una imagen.")
-    else:        
-        await ctx.send("No encuentro ningún archivo")
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f"file/{file_name}")
+            await ctx.send(f"Gracias por subir la imagen! La URL de tu imagen es: {file_url}")
+            await ctx.send(get_class(model_path="./keras_model.h5", labels_path="./labels.txt", image_path=f"file/{file_name}"))
+    else:
+        await ctx.send("No recibí ninguna imagen. Por favor, intenta de nuevo.")
         
 bot.run("")
-
     
